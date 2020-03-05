df70d5b30ed3411d7cfc90c17cf4327dc269b0f9
Riley Slayden <slaydenriley@gmail.com>
2020-03-05 16:50:12 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :sessions
-  resources :welcome, only: [:index]
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+  root 'welcome#home'
 end

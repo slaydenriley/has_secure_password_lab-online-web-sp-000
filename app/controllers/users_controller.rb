a77c096b85175116e1e8c8d0e3444557cd2a7362
Riley Slayden <slaydenriley@gmail.com>
2020-03-05 16:48:42 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
+  def new
+  end
+
   def create
-    @user = User.new(name: params[:name], password_digest: params[:password])
-    @user.save
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'welcome', action: 'home'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
